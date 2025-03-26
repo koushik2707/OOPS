91a6b5ed9e12820d727fca8be1a1a8d565a0dfef
koushik2707 <kalvakoushik5@gmail.com>
2025-03-26 12:23:02 +0530

    Add files via upload

--- vehicledemo.java
+++ vehicledemo.java
@@ -1,23 +1,21 @@
 class vehicle{
     void displayInfo(){
-        System.out.println("this is a vehicle");
+        System.out.println("This is a vehicle");
     }
 }
 class car extends vehicle{
-        void dispayInfo(){   
-            String company = "ferrari";
-            String fuel_type = "petrol";
-            String numberplate = "AP2307K2007";
+        void dispayInfo(String company,String fuel_type,String numberplate){  
+            System.out.println("This is my car");
             System.out.println("Brand:" + company);
             System.out.println("Fuel type:" + fuel_type);
             System.out.println("Number Plate:" + numberplate);
     }
 }
 class vehicledemo {
-    public static void main (Strings [] args){
+    public static void main (String[] args){
         vehicle v = new vehicle();
         v.displayInfo();
         car c = new car();
-        c.dispayInfo();
+        c.dispayInfo("Ferrari","Petrol","TS15K2707");
     }
 }
